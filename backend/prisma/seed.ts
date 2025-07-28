656f7887b5b12996b9b9ce9febcf0e1e64540ea0
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-07-28 16:26:05 +0900

    migrate:DBにUserを反映

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const email = 'admin@example.com';
+  const passwordHash = await bcrypt.hash('AdminnPass123!', 10);
+  const admin = await prisma.user.upsert({
+    where: { email },
+    update: {},
+    create: { email, passwordHash },
+  });
+
+  await prisma.todo.updateMany({
+    data: { userId: admin.id },
+    where: { userId: { equals: null } },
+  });
+}
+
+main().finally(() => prisma.$disconnect());

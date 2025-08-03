0b25935fa41b4389c6bc129470caa6c22c3f338f
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-08-03 16:58:22 +0900

    add:User用のモジュールを追加

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  findById(id: number) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async create(email: string, passwordHash: string) {
+    return this.prisma.user.create({ data: { email, passwordHash } });
+  }
+}
